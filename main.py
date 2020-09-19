2466bcc09324aef24391e3da1da8bb945b0037b3
Donny Son <dongook.son@yonsei.ac.kr>
2020-09-19 11:48:45 +0900

    Python logging example Initial commit;

---
+++ main.py
@@ -0,0 +1,11 @@
+import library  
+import logging
+import someloop
+
+logger = logging.getLogger(__name__)
+
+logger.info('this is from main')
+
+logger.info('calling foo from someloop.py')
+someloop.foo()
+
